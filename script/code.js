71e4bebbf35625d49c617be929818ab441e742fb
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-05-14 14:32:36 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,20 @@
+// Question 1 & 2
+let fisrtname = "Awodwa"
+let lastname = "Hendrick"
+
+let concat = fisrtname+" "+lastname
+
+console.log(concat)
+
+// Question3 & 4
+
+let numberVar = 5
+
+console.log(numberVar*5)
+
+// Question5  
+
+const surname = "Hendrick"
+name = "Julie"
+console.log(name)
+
